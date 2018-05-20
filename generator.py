09975aa3223c691e3d5ee8c26cb4e09eef4132bc
Lukas Bernhard <strange85@web.de>
2018-05-20 19:14:57 +0200

    cleaning up

--- generator.py
+++ generator.py
@@ -1,15 +1,12 @@
 import numpy as np
 from scipy.io.wavfile import write
-from scipy.io.wavfile import read
-from scipy.fftpack import fft
-from scipy.fftpack import ifft
 import matplotlib.pyplot as plt
 
 
 VOLUME = 15000
 AUDIO_LEN = 1  # in sec
 MAX_FREQ = 22000  # in hz
-SAMPLE_FREQ = MAX_FREQ #* 2 + 100  # in hz
+SAMPLE_FREQ = MAX_FREQ  # * 2 + 100  # in hz
 SAMPLES = SAMPLE_FREQ * AUDIO_LEN  # N
 TONE = 440  # in hz
 
@@ -33,52 +30,42 @@ def generate_note(f_root):
     num_overtones = 5  # int(MAX_FREQ / TONE) - 1
 
     # init arrays
-    time_line = np.linspace(0, AUDIO_LEN, SAMPLES)
     f_spec = np.linspace(f_root, MAX_FREQ, MAX_FREQ - f_root)  # array with freq from main tone to max freq
-    f_prop = np.zeros(f_spec.shape)
-    note = np.zeros(time_line.shape)
+    signal_fft = np.zeros(f_spec.shape)
 
     # fill weights of overtones
     for i in range(0, num_overtones):
-        gaus = gaussian(f_spec, f_spec[i*f_root]) * np.exp(-i*0.05) * 100
-        f_prop += gaus
-        # f_prop[i*f_root] = 1
+        signal_fft += gaussian(f_spec, f_spec[i*f_root]) * np.exp(-i * 0.05) * 100
 
     # generate tone
-    a = np.pi * time_line  # outside for-loop for performance
-    for i in range(0, len(f_spec)):
-        if f_prop[i] != 0.0:
-            note += np.sin(f_spec[i] * a) * float(f_prop[i]) * fade_out(time_line, f_spec[i])
-    return note, f_prop
+    tone = np.fft.ifft(signal_fft)
+    tone = np.int16( tone/np.max(np.abs(tone)) * VOLUME)  # normalize and scale for volume
+    return tone, signal_fft
 
 
-def display(tone, weights):
+def display(signal, signal_fft):
+    f, axarr = plt.subplots(5)
     t = AUDIO_LEN / SAMPLES
-    xf = np.linspace(0.0, 1.0 / (2.0 * t), MAX_FREQ // 2)
-    yf = fft(tone)
 
+    # create x aches for time, weights and fourier
+    xf = np.linspace(0.0, 1.0 / (2.0 * t), MAX_FREQ // 2)
     xw = np.linspace(TONE, MAX_FREQ, MAX_FREQ - TONE)
     xt = np.linspace(0, AUDIO_LEN, SAMPLES)
 
-    f, axarr = plt.subplots(5)
-    axarr[0].plot(xt, tone)
+    # plot everything
+    axarr[0].plot(xt, signal)
     axarr[0].set_title('Ton')
     axarr[0].grid()
-    axarr[1].plot(xf, 2.0/MAX_FREQ * np.abs(yf[0:MAX_FREQ//2]))
+
+    axarr[1].plot(xf, 2.0/MAX_FREQ * np.abs(signal_fft[0:MAX_FREQ//2]))
     axarr[1].set_title('FFT')
     axarr[1].grid()
-    axarr[2].plot(xw, weights)
+
+    axarr[2].plot(xw, signal_fft)
     axarr[2].set_title('Weights')
     axarr[2].grid()
-
-    rate, guitar = read('guitar.wav')
-    g_yf = fft(guitar)
-    axarr[3].plot(xf, 2.0/MAX_FREQ * np.abs(g_yf[0:MAX_FREQ//2]))
-    axarr[3].set_title('Guitar')
-    axarr[3].grid()
     plt.show()
 
-data, weight = generate_note(TONE)
-scaled = np.int16( data/np.max(np.abs(data)) * VOLUME)  # normalize and scale for volume
-write('test.wav', SAMPLES, scaled)
-display(scaled, weight)
+signal, frequencies = generate_note(TONE)
+write('samples/generated.wav', SAMPLES, signal)
+display(signal, frequencies)
